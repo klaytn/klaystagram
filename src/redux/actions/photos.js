7a7d5bc22feff75ceb831ef00a6f82043597d867
Edwin <112916339+edwin082@users.noreply.github.com>
2022-10-14 14:57:40 +0900

    Update photos.js

--- src/redux/actions/photos.js
+++ src/redux/actions/photos.js
@@ -4,7 +4,6 @@ import ui from 'utils/ui'
 import { feedParser } from 'utils/misc'
 import { SET_FEED } from './actionTypes'
 
-
 // Action creators
 
 const setFeed = (feed) => ({
@@ -17,6 +16,8 @@ const updateFeed = (tokenId) => (dispatch, getState) => {
     .then((newPhoto) => {
       const { photos: { feed } } = getState()
       const newFeed = [feedParser(newPhoto), ...feed]
+
+      // Update new feed to store
       dispatch(setFeed(newFeed))
     })
 }
@@ -24,13 +25,12 @@ const updateFeed = (tokenId) => (dispatch, getState) => {
 const updateOwnerAddress = (tokenId, to) => (dispatch, getState) => {
   const { photos: { feed } } = getState()
   const newFeed = feed.map((photo) => {
-    if (photo[ID] !== tokenId) return photo
-    photo[OWNER_HISTORY].push(to)
+    if (photo['id'] !== tokenId) return photo
+    photo['ownerHistory'] = [...photo['ownerHistory'], to]
     return photo
   })
   dispatch(setFeed(newFeed))
 }
-
 
 // API functions
 
@@ -54,70 +54,91 @@ export const uploadPhoto = (
   location,
   caption
 ) => (dispatch) => {
+  // 1. Convert photo file as a hex string to load on transaction
   const reader = new window.FileReader()
   reader.readAsArrayBuffer(file)
   reader.onloadend = () => {
     const buffer = Buffer.from(reader.result)
-    /**
-     * Add prefix `0x` to hexString
-     * to recognize hexString as bytes by contract
-     */
+
+    // Add prefix `0x` to hexString to recognize hexString as bytes by contract
     const hexString = "0x" + buffer.toString('hex')
-    KlaystagramContract.methods.uploadPhoto(hexString, fileName, location, caption).send({
-      from: getWallet().address,
-      gas: '200000000',
-    })
-      .once('transactionHash', (txHash) => {
+
+    // 2. Invoke the contract method: uploadPhoto
+    // Send transaction with photo file(hexString) and descriptions
+    try{
+      KlaystagramContract.methods.uploadPhoto(hexString, fileName, location, caption).send({
+        from: getWallet().address,
+        gas: '200000000',
+      }, (error, txHash) => {
+        if (error) throw error;
+
+        // 3. After sending the transaction,
+        // show progress along the transaction lifecycle using `Toast` component.
         ui.showToast({
           status: 'pending',
           message: `Sending a transaction... (uploadPhoto)`,
           txHash,
         })
       })
-      .once('receipt', (receipt) => {
-        ui.showToast({
-          status: receipt.status ? 'success' : 'fail',
-          message: `Received receipt! It means your transaction is
-          in klaytn block (#${receipt.blockNumber}) (uploadPhoto)`,
-          link: receipt.transactionHash,
+        .then((receipt) => {
+          ui.showToast({
+            status: receipt.status ? 'success' : 'fail',
+            message: `Received receipt! It means your transaction is
+            in klaytn block (#${receipt.blockNumber}) (uploadPhoto)`,
+            link: receipt.transactionHash,
+          })
+
+          // 4. If the transaction successfully gets into a block,
+          // call `updateFeed` function to add the new photo into the feed page.
+          if(receipt.status) {
+            const tokenId = receipt.events.PhotoUploaded.returnValues[0]
+            dispatch(updateFeed(tokenId))
+          }
         })
-        const tokenId = receipt.events.PhotoUploaded.returnValues[0]
-        dispatch(updateFeed(tokenId))
+    } catch (error) {
+      ui.showToast({
+        status: 'error',
+        message: error.toString(),
       })
-      .once('error', (error) => {
-        ui.showToast({
-          status: 'error',
-          message: error.toString(),
-        })
-      })
+    }
   }
 }
 
 export const transferOwnership = (tokenId, to) => (dispatch) => {
-  KlaystagramContract.methods.transferOwnership(tokenId, to).send({
-    from: getWallet().address,
-    gas: '20000000',
-  })
-    .once('transactionHash', (txHash) => {
+  // 1. Invoke the contract method: transferOwnership
+  try{
+    KlaystagramContract.methods.transferOwnership(tokenId, to).send({
+      
+      // 2. Set transaction options
+      from: getWallet().address,
+      gas: '20000000',
+    }, (error, txHash) => {
+      if (error) throw error;
+
+      // 3. After sending the transaction,
+      // show progress along the transaction lifecycle using `Toast` component.
       ui.showToast({
         status: 'pending',
         message: `Sending a transaction... (transferOwnership)`,
         txHash,
       })
     })
-    .once('receipt', (receipt) => {
-      ui.showToast({
-        status: receipt.status ? 'success' : 'fail',
-        message: `Received receipt! It means your transaction is
-          in klaytn block (#${receipt.blockNumber}) (transferOwnership)`,
-        link: receipt.transactionHash,
+      .then((receipt) => {
+        ui.showToast({
+          status: receipt.status ? 'success' : 'fail',
+          message: `Received receipt! It means your transaction is
+            in klaytn block (#${receipt.blockNumber}) (transferOwnership)`,
+          link: receipt.transactionHash,
+        })
+
+        // 4. If the transaction successfully gets into a block,
+        // call `updateOwnerAddress` function to update new owner's address into the feed page.
+        dispatch(updateOwnerAddress(tokenId, to))
       })
-      dispatch(updateOwnerAddress(tokenId, to))
+  } catch (error) {
+    ui.showToast({
+      status: 'error',
+      message: error.toString(),
     })
-    .once('error', (error) => {
-      ui.showToast({
-        status: 'error',
-        message: error.toString(),
-      })
-    })
+  }
 }
